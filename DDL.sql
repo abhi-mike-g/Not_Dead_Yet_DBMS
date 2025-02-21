3b67264d501000bf14ea303c06e2aa134abb32f0
AMG <abhimg20@gmail.com>
2025-02-21 09:51:20 +0530

    Create DDL.sql

---
+++ DDL.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE HMS;
+USE HMS;
+
+CREATE TABLE Patient(
+email varchar(50) PRIMARY KEY,
+password varchar(30) NOT NULL,
+name varchar(50) NOT NULL,
+address varchar(60) NOT NULL,
+gender VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE MedicalHistory(
+id int PRIMARY KEY,
+date DATE NOT NULL,
+conditions VARCHAR(100) NOT NULL, 
+surgeries VARCHAR(100) NOT NULL, 
+medication VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Doctor(
+email varchar(50) PRIMARY KEY,
+gender varchar(20) NOT NULL,
+password varchar(30) NOT NULL,
+name varchar(50) NOT NULL
+);
+
+CREATE TABLE Appointment(
+id int PRIMARY KEY,
+date DATE NOT NULL,
+starttime TIME NOT NULL,
+endtime TIME NOT NULL,
+status varchar(15) NOT NULL
+);
